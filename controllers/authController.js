be155da6cbd5d97159c930bf8778dc6b7e833037
Gyana Singh <gyan123priya@gmail.com>
2025-12-14 02:31:05 +0530

    hii

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,30 +1,5 @@
 const { signToken } = require("../utils/jwt");
 
-// exports.googleAuthSuccess = async (req, res) => {
-//   if (!req.user) {
-//     return res.status(400).json({ message: "Google auth failed" });
-//   }
-
-//   const token = signToken(req.user);
-
-//   res.json({
-//     message: "Google Login Successful",
-//     user: req.user,
-//     token,
-//   });
-// };
-
-// exports.googleAuthSuccess = async (req, res) => {
-//   if (!req.user) {
-//     return res.redirect("http://localhost:3000/login?error=google");
-//   }
-
-//   const token = signToken(req.user);
-
-//   // ✅ redirect to Next.js with token
-//   res.redirect(`http://localhost:3000/auth/google/success?token=${token}`);
-// };
-
 exports.googleAuthSuccess = async (req, res) => {
   try {
     if (!req.user) {
@@ -34,9 +9,7 @@ exports.googleAuthSuccess = async (req, res) => {
     const token = signToken(req.user);
 
     // ✅ redirect to frontend
-    res.redirect(
-      `http://localhost:3000/auth/google/success?token=${token}`
-    );
+    res.redirect(`http://localhost:3000/auth/google/success?token=${token}`);
   } catch (error) {
     console.error("Auth Success Error:", error);
     res.status(500).json({ message: "Internal Server Error" });
