4e33d7cc54ba359a32f775641f1cbb79f49e00f3
Gyana Singh <gyan123priya@gmail.com>
2025-12-15 23:32:03 +0530

    hii

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,15 +2,13 @@ const { signToken } = require("../utils/jwtAuth");
 
 exports.googleAuthSuccess = async (req, res) => {
   if (!req.user) {
-    return res.redirect(
-      `${process.env.CLIENT_URL}/Login?error=google`
-    );
+    return res.redirect(`${process.env.CLIENT_URL}/Login?error=google`);
   }
 
   const token = signToken(req.user);
 
   // ✅ Redirect to Vercel frontend
   res.redirect(
-    `${process.env.CLIENT_URL}/auth/google/success?token=${token}`
+    `https://chat-app-admin-dashboard-b3ut.vercel.app/auth/google/success?token=${token}`
   );
 };
