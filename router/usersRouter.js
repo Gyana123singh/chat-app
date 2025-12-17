127ce4ed23e1bd036a8a6f4bacc4fb86b105ca6e
Gyana Singh <gyan123priya@gmail.com>
2025-12-17 19:21:24 +0530

    hii

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,18 +1,16 @@
 // routes/users.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const userPhoneOtpAuth = require('../controllers/authController');
-const { authMiddleware } = require('../middleware/auth');
+const userController = require("../controllers/userController");
+const userPhoneOtpAuth = require("../controllers/authController");
+const { authMiddleware } = require("../middleware/auth");
 
-
-router.post('/phone-login',authMiddleware, userPhoneOtpAuth.phoneOtpAuth);
-router.get('/profile', authMiddleware, userController.getProfile);
-router.put('/profile', authMiddleware, userController.updateProfile);
-router.get('/:id', userController.getUserById);
-router.get('/search', userController.searchUsers);
-router.post('/follow/:id', authMiddleware, userController.followUser);
-router.get('/followers', authMiddleware, userController.getFollowers);
-router.get('/following', authMiddleware, userController.getFollowing);
+router.get("/profile", authMiddleware, userController.getProfile);
+router.put("/profile", authMiddleware, userController.updateProfile);
+router.get("/:id", userController.getUserById);
+router.get("/search", userController.searchUsers);
+router.post("/follow/:id", authMiddleware, userController.followUser);
+router.get("/followers", authMiddleware, userController.getFollowers);
+router.get("/following", authMiddleware, userController.getFollowing);
 
 module.exports = router;
