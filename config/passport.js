fdca722ff761cf4077ae70ee54f3ffcfbceb79f7
Gyana Singh <gyan123priya@gmail.com>
2025-12-19 10:32:36 +0530

    hii

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: "https://chat-app-1-qvl9.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
