6e3d769d3318e663c0915b2bda9aa4144fad7c35
Gyana Singh <gyan123priya@gmail.com>
2025-12-27 13:01:54 +0530

    Hii

--- router/roomsRouter.js
+++ router/roomsRouter.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const roomController = require("../controllers/roomController");
 const { authMiddleware } = require("../middleware/auth");
 
+router.get("/get-all-rooms", roomController.getAllRooms); //done get all rooms with pagination, search, filter
 router.get("/my-rooms", authMiddleware, roomController.getMyRooms);
 router.get("/:roomId", authMiddleware, roomController.getRoomById); //done get data user by id
 router.post("/create", authMiddleware, roomController.createRoom); //done create room
-router.get("/get-all-rooms", roomController.getAllRooms); //done get all rooms with pagination, search, filter
 router.get("/popular", roomController.getPopularRooms);
 router.put("/:id", authMiddleware, roomController.updateRoom);
 router.delete("/:id", authMiddleware, roomController.deleteRoom);
