d52c2243d37ae1d4b4124d644352bd5ba0b81e6b
Gyana Singh <gyan123priya@gmail.com>
2025-12-29 18:20:32 +0530

    hii

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,8 @@ exports.googleAuthSuccess = async (req, res) => {
   const token = signToken(req.user);
 
   // ✅ Redirect to Vercel frontend
-  res.redirect(`myapp://auth/google/success?token=${token}`);
+  // res.redirect(`myapp://auth/google/success?token=${token}`);
+  res.redirect(`http://localhost:3001/auth/google/success?token=${token}`);
 
 };
 
