41ab59d0b4af32eecf0a7ddf6db08fa3ba69c0d6
Gyana Singh <gyan123priya@gmail.com>
2026-01-09 18:14:32 +0530

    hii

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://test-admin-chat-nine.vercel.app",
+    origin: true,
     methods: ["GET", "POST"],
     credentials: true,
   },
