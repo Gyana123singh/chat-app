35513d1815e91257c02b8d28d21ecacbe99a2ea8
Gyana Singh <gyan123priya@gmail.com>
2026-01-17 02:36:05 +0530

    hii

--- server.js
+++ server.js
@@ -173,54 +173,54 @@ app.get("/stream/:roomId/:filename", (req, res) => {
 
 /* ===================== VIDEO ROUTES ===================== */
 
-const videoRouter = require("./router/videoRouter")(io);
-app.use("/api/video", videoRouter);
+// const videoRouter = require("./router/videoRouter")(io);
+// app.use("/api/video", videoRouter);
 
-/* ===================== VIDEO STREAM ROUTE ===================== */
 
-app.get("/video-stream/:roomId/:filename", (req, res) => {
-  const filePath = path.resolve(
-    process.cwd(),
-    "uploads",
-    "videos",
-    req.params.roomId,
-    req.params.filename
-  );
 
-  if (!fs.existsSync(filePath)) {
-    console.log("❌ Video file not found:", filePath);
-    return res.status(404).end();
-  }
+// app.get("/video-stream/:roomId/:filename", (req, res) => {
+//   const filePath = path.resolve(
+//     process.cwd(),
+//     "uploads",
+//     "videos",
+//     req.params.roomId,
+//     req.params.filename
+//   );
 
-  const stat = fs.statSync(filePath);
-  const fileSize = stat.size;
-  const range = req.headers.range;
+//   if (!fs.existsSync(filePath)) {
+//     console.log("❌ Video file not found:", filePath);
+//     return res.status(404).end();
+//   }
 
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+//   const stat = fs.statSync(filePath);
+//   const fileSize = stat.size;
+//   const range = req.headers.range;
 
-    const chunkSize = end - start + 1;
-    const file = fs.createReadStream(filePath, { start, end });
+//   if (range) {
+//     const parts = range.replace(/bytes=/, "").split("-");
+//     const start = parseInt(parts[0], 10);
+//     const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
 
-    res.writeHead(206, {
-      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunkSize,
-      "Content-Type": "video/mp4",
-    });
+//     const chunkSize = end - start + 1;
+//     const file = fs.createReadStream(filePath, { start, end });
 
-    file.pipe(res);
-  } else {
-    res.writeHead(200, {
-      "Content-Length": fileSize,
-      "Content-Type": "video/mp4",
-    });
+//     res.writeHead(206, {
+//       "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+//       "Accept-Ranges": "bytes",
+//       "Content-Length": chunkSize,
+//       "Content-Type": "video/mp4",
+//     });
 
-    fs.createReadStream(filePath).pipe(res);
-  }
-});
+//     file.pipe(res);
+//   } else {
+//     res.writeHead(200, {
+//       "Content-Length": fileSize,
+//       "Content-Type": "video/mp4",
+//     });
+
+//     fs.createReadStream(filePath).pipe(res);
+//   }
+// });
 
 require("./middleware/soket.middleware")(io);
 require("./utils/socketEvents")(io);
