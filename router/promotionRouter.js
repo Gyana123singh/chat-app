40fe0bf7a82f9caefea13074c3a03d46a7f33660
Gyana Singh <gyan123priya@gmail.com>
2026-01-19 13:00:22 +0530

    hii

--- router/promotionRouter.js
+++ router/promotionRouter.js
@@ -5,35 +5,48 @@ const router = express.Router();
 const upload = require("../middleware/multer.middleware");
 const { authMiddleware } = require("../middleware/auth");
 
-const controller = require("../models/notification");
+const notificationController = require("../models/notification");
 
 // BANNER
 router.post(
   "/create-banner",
   authMiddleware,
   upload.single("file"),
-  controller.createBanner,
+  notificationController.createBanner,
 );
 
 // OFFER
-router.post("/create-offer", authMiddleware, controller.createOffer);
+router.post(
+  "/create-offer",
+  authMiddleware,
+  notificationController.createOffer,
+);
 
 // AD
 router.post(
   "/create-ads",
   authMiddleware,
   upload.single("file"),
-  controller.createAd,
+  notificationController.createAd,
 );
 
 // POPUP
-router.post("/create-popup", authMiddleware, controller.createPopup);
+router.post(
+  "/create-popup",
+  authMiddleware,
+  notificationController.createPopup,
+);
 
 // PUBLIC APIs (no auth)
-router.get("/get-banners", controller.getBanners);
-router.get("/get-offers", controller.getOffers);
-router.get("/get-ads", controller.getAds);
-router.get("/get-popups", authMiddleware, getPopups);
-router.post("/popup/seen", authMiddleware, markPopupSeen);
+router.get("/get-banners", notificationController.getBanners);
+router.get("/get-offers", notificationController.getOffers);
+router.get("/get-ads", notificationController.getAds);
+router.get("/get-popups", authMiddleware, notificationController, getPopups);
+router.post(
+  "/popup/seen",
+  authMiddleware,
+  notificationController,
+  markPopupSeen,
+);
 
 module.exports = router;
