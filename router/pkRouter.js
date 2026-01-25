43b39eda64cae8a394b089943b2a416189b6b9e6
Gyana Singh <gyan123priya@gmail.com>
2026-01-25 23:44:46 +0530

    hii

--- router/pkRouter.js
+++ router/pkRouter.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { createPK } = require("../controllers/pkController");
-const middleware = require("../middleware/auth"); // your JWT middleware
+const { authMiddleware } = require("../middleware/auth"); // your JWT middleware
 
 // ==========================
 // 🔥 CREATE PK
 // ==========================
 // Only room host can start PK
 // POST /api/pk/create
-router.post("/create-pk", middleware, createPK);
+router.post("/create-pk", authMiddleware, createPK);
 
 module.exports = router;
