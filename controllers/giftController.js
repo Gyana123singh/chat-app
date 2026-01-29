6da0c33ecc4853da6d33af6ef8b58faf9522760a
Gyana Singh <gyan123priya@gmail.com>
2026-01-29 17:26:48 +0530

    hii

--- controllers/giftController.js
+++ controllers/giftController.js
@@ -205,7 +205,7 @@ exports.checkEligibility = async (req, res) => {
 
 exports.sendGift = async (req, res) => {
   try {
-    const senderId = req.user.sub;
+    const senderId = req.user.id;
     const {
       roomId,
       giftId,
