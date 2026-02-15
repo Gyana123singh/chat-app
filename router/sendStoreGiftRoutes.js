3344eaa148c812f11d9977716ba0e20441b5e60c
Gyana Singh <gyan123priya@gmail.com>
2026-02-15 14:08:38 +0530

    hii

--- router/sendStoreGiftRoutes.js
+++ router/sendStoreGiftRoutes.js
@@ -4,6 +4,6 @@ const giftSendController = require("../controllers/storeGiftSendController");
 const { authMiddleware } = require("../middleware/auth");
 
 // Send gift operations
-router.post("/send-gifts", authMiddleware, giftSendController.sendGift);
+router.post("/send-gift", authMiddleware, giftSendController.sendGift);
 
 module.exports = router;
