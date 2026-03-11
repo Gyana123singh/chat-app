b7716219c2cf019acbd062fdce34176eda04be07
Gyana Singh <gyan123priya@gmail.com>
2026-03-11 18:07:50 +0530

    hii

--- utils/socketEvents.js
+++ utils/socketEvents.js
@@ -281,7 +281,7 @@ module.exports = (io) => {
             },
           }));
 
-        io.to(roomName).emit("room:users", usersInRoom);
+        socket.emit("room:users", usersInRoom);
 
         /* ===== MESSAGES ===== */
         const messages = roomMessages.get(roomId) || [];
