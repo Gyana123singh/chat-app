382303f687bb66a4653624b98517eb2284f3c228
Gyana Priyadarsan Singh <gyan123priya@gmail.com>
2026-05-06 16:24:08 +0530

    hii

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
+
 const connectMongose = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB connected successfully");
+    await mongoose.connect(process.env.MONGO_URL, {
+      family: 4,
+      serverSelectionTimeoutMS: 60000,
+      socketTimeoutMS: 60000,
+    });
+
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error);
+    console.error("❌ MongoDB connection failed:");
+    console.error(error);
   }
 };
 
