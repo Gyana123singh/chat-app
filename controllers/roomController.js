640cd191e296ee95e2eff9e1c740291e74884851
AbhayAnand26 <139357233+AbhayAnand26@users.noreply.github.com>
2026-06-19 18:49:01 +0530

    hiii

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -50,12 +50,14 @@ exports.createRoom = async (req, res) => {
     const existingRoom = await Room.findOne({
       creator: userId,
       isActive: true,
-    });
+    }).populate("participants.user", "username avatar");
 
     if (existingRoom) {
-      return res.status(400).json({
-        success: false,
+      return res.status(200).json({
+        success: true,
         message: "You already have an active room",
+        existingRoom: true,
+        room: existingRoom,
         roomId: existingRoom.roomId,
       });
     }
